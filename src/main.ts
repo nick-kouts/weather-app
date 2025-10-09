2cc621b045117a81660df04e0633d9ef9cba0441
Nick Koutsoulidakis <nickkoutsoulidakis@gmail.com>
2025-10-09 07:10:41 -0400

    Flattened repo structure and removed legacy folder - again

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { AppComponent } from './assets/app/app.component';
+import { AppComponent } from './app/app.component'
 import { provideHttpClient } from '@angular/common/http';
-import { routes } from './assets/app/app.routes';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [ provideHttpClient(), provideRouter(routes) ] 
